4b6a3c4986dda332dd476e81ecec247bb4bf2cc4
ThEVlaDGamE <thevlad2005@yandex.ru>
2022-08-09 00:54:10 +0300

    Создание алгоритма для оконного приложения

--- WordFinder/WordFinder/Form1.Designer.cs
+++ WordFinder/WordFinder/Form1.Designer.cs
@@ -33,32 +33,38 @@
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.listBox1 = new System.Windows.Forms.ListBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.tableLayoutPanel1.SuspendLayout();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
             // 
             this.tableLayoutPanel1.ColumnCount = 1;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.groupBox4, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.groupBox3, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 4);
             this.tableLayoutPanel1.Controls.Add(this.groupBox2, 0, 1);
-            this.tableLayoutPanel1.Controls.Add(this.groupBox3, 0, 2);
             this.tableLayoutPanel1.Controls.Add(this.groupBox1, 0, 0);
-            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 3);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 4;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.94737F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.47368F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 46.31579F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.090909F));
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.10526F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 24.21053F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 21.89474F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 32F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.36842F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.tableLayoutPanel1.Size = new System.Drawing.Size(436, 475);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
@@ -68,7 +74,7 @@
             this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.groupBox1.Location = new System.Drawing.Point(3, 3);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(430, 46);
+            this.groupBox1.Size = new System.Drawing.Size(430, 42);
             this.groupBox1.TabIndex = 3;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Количество символов (если неизвестно, 0)";
@@ -79,6 +85,7 @@
             this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.numericUpDown1.Location = new System.Drawing.Point(3, 16);
             this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.ReadOnly = true;
             this.numericUpDown1.Size = new System.Drawing.Size(424, 30);
             this.numericUpDown1.TabIndex = 1;
             // 
@@ -86,12 +93,12 @@
             // 
             this.groupBox2.Controls.Add(this.textBox1);
             this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.groupBox2.Location = new System.Drawing.Point(3, 55);
+            this.groupBox2.Location = new System.Drawing.Point(3, 51);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(430, 153);
+            this.groupBox2.Size = new System.Drawing.Size(430, 109);
             this.groupBox2.TabIndex = 4;
             this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Буквы (в определённом месте или в любом месте)";
+            this.groupBox2.Text = "Буквы (в определённом месте)";
             // 
             // textBox1
             // 
@@ -100,18 +107,30 @@
             this.textBox1.Location = new System.Drawing.Point(3, 16);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(424, 134);
+            this.textBox1.Size = new System.Drawing.Size(424, 90);
             this.textBox1.TabIndex = 2;
-            this.textBox1.Text = "1: н\r\n4: е\r\n5: т\r\nа, р\r\n";
+            this.textBox1.Text = "1: н\r\n4: е\r\n5: т\r\n";
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(3, 422);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(430, 50);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Найти";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // groupBox3
             // 
             this.groupBox3.Controls.Add(this.listBox1);
             this.groupBox3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.groupBox3.Location = new System.Drawing.Point(3, 214);
+            this.groupBox3.Location = new System.Drawing.Point(3, 270);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(430, 214);
-            this.groupBox3.TabIndex = 5;
+            this.groupBox3.Size = new System.Drawing.Size(430, 146);
+            this.groupBox3.TabIndex = 8;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Результат";
             // 
@@ -123,20 +142,30 @@
             this.listBox1.ItemHeight = 20;
             this.listBox1.Location = new System.Drawing.Point(3, 16);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(424, 195);
+            this.listBox1.Size = new System.Drawing.Size(424, 127);
             this.listBox1.TabIndex = 3;
             // 
-            // button1
+            // groupBox4
             // 
-            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(3, 434);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(430, 38);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "Найти";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.groupBox4.Controls.Add(this.textBox2);
+            this.groupBox4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox4.Location = new System.Drawing.Point(3, 166);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(430, 98);
+            this.groupBox4.TabIndex = 9;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Буквы (в любом месте)";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(3, 16);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(424, 79);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "а, р\r\n\r\n";
             // 
             // Form1
             // 
@@ -152,6 +181,8 @@
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
             this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -161,10 +192,12 @@
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.GroupBox groupBox3;
-        private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button button1;
     }
 }
--- WordFinder/WordFinder/Form1.cs
+++ WordFinder/WordFinder/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,14 +13,162 @@ namespace WordFinder
 {
     public partial class Form1 : Form
     {
+        string path = "RUS.txt";
+
         public Form1()
         {
             InitializeComponent();
         }
 
+        bool CheckWordSymbolsCount(int rule, int value)
+        {
+            if (rule == 0)
+            {
+                return true;
+            }
+            else
+            {
+                if (rule == value)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        bool CheckWordDefiniteSymbol(DefiniteSymbol[] rules, char[] word)
+        {
+            if (rules.Length > 0)
+            {
+                int counter = 0;
+                for(int i = 0; i < rules.Length; i++)
+                {
+                    if (rules[i].index < word.Length)
+                    {
+                        if (rules[i].symbol.ToLower() == word[rules[i].index].ToString().ToLower())
+                        {
+                            counter++;
+                        }
+                    }
+                }
+
+                if (counter == rules.Length)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return true;
+            }
+        }
+        bool CheckWordFreeSymbol(string[] rules, char[] word)
+        {
+            if (rules.Length > 0)
+            {
+                int counter = 0;
+                for (int i = 0; i < rules.Length; i++)
+                {
+                    for(int j = 0; j < word.Length; j++)
+                    {
+                        if (rules[i].ToLower() == word[j].ToString().ToLower())
+                        {
+                            counter++;
+                            break;
+                        }
+                    }
+                }
+
+                if (counter == rules.Length)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        struct DefiniteSymbol
+        {
+            public int index;
+            public string symbol;
+
+            public DefiniteSymbol(int index, string symbol)
+            {
+                this.index = index;
+                this.symbol = symbol;
+            }
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
+            if ((textBox1.Text != "") || (textBox2.Text != ""))
+            {
+                int wordSymbolsCount = int.Parse(numericUpDown1.Value.ToString());
 
+                string[] definiteSymbols1;
+                DefiniteSymbol[] definiteSymbols = new DefiniteSymbol[0];
+                if (textBox1.Text != "")
+                {
+                    definiteSymbols1 = textBox1.Text.Split('\n');
+                    definiteSymbols = new DefiniteSymbol[definiteSymbols1.Length];
+
+                    for (int i = 0; i < definiteSymbols1.Length; i++)
+                    {
+                        string[] definiteSymbols2 = definiteSymbols1[i].Split(':');
+
+                        definiteSymbols[i] = new DefiniteSymbol(int.Parse(definiteSymbols2[0].Trim()), definiteSymbols2[1].ToString().Trim().ToLower());
+                    }
+                }
+
+
+                string[] freeSymbols = new string[0];
+                if (textBox2.Text != "")
+                {
+                    freeSymbols = textBox2.Text.Split(',');
+                    for (int i = 0; i < freeSymbols.Length; i++)
+                    {
+                        freeSymbols[i] = freeSymbols[i].Trim().ToLower();
+                    }
+                }
+
+
+                using (StreamReader reader = new StreamReader(path))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        char[] letters = line.ToCharArray();
+                        if (CheckWordSymbolsCount(wordSymbolsCount, letters.Length))
+                        {
+                            if (CheckWordDefiniteSymbol(definiteSymbols, letters))
+                            {
+                                if (CheckWordFreeSymbol(freeSymbols, letters))
+                                {
+                                    listBox1.Items.Add(line);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Введите условия поиска");
+            }
         }
     }
 }
